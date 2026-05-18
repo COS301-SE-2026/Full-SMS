023d0b71eb766631ffda6b29d7552dd803892eea
carlos-juma <u22602047@tuks.co.za>
2026-05-18 13:32:21 +0200

    add component exports

---
+++ frontend/components/ui/index.ts
@@ -0,0 +1,13 @@
+export { Button }                                          from './Button'
+export type { ButtonProps }                                from './Button'
+
+export { Input }                                           from './Input'
+export type { InputProps }                                 from './Input'
+
+export { Toggle }                                          from './Toggle'
+export type { ToggleProps }                                from './Toggle'
+
+export { Badge }                                           from './Badge'
+export type { BadgeProps }                                 from './Badge'
+
+export { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from './Card'
