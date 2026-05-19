713fad2fbb94cb382076538d3bb82e6d0b2bcf85
makopimbalithobejane@gmail.com <makopimbalithobejane@gmail.com>
2026-05-19 16:09:12 +0200

    login page base structure

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+
+
+export default function LoginPage() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-background px-4 dark">
+           <p className="text-foreground">Login page</p>
+        </div>
+    )
+}
