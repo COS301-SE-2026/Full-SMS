b1cf8aa45676ec5cea2f1a5f3a1bdbb9ac18e4d1
makopimbalithobejane@gmail.com <makopimbalithobejane@gmail.com>
2026-05-19 17:41:09 +0200

    created registration base structure

---
+++ frontend/app/register/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+
+export default function RegisterPage() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-background px-4 dark">
+            <p className="text-foreground">Register page</p>
+        </div>
+    )
+}
