e7e9a2c4ec17464011ff1f61aee7fde62222de61
carlos-juma <u22602047@tuks.co.za>
2026-05-22 01:47:11 +0200

    chore: update git ignore

--- api/routes/upload.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, UploadFile, File
-from controllers.upload_controller import handle_upload
-
-router = APIRouter(prefix="/upload", tags=["upload"])
-
-@router.post("/")
-async def upload_file(file: UploadFile = File(...)):
-    return await handle_upload(file)
