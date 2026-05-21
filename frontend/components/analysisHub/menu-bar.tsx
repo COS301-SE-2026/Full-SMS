7ab5b641538eeb642ddd8615dc90fecfc94481cc
carlos-juma <u22602047@tuks.co.za>
2026-05-22 01:41:38 +0200

    feat: add menu bar

---
+++ frontend/components/analysisHub/menu-bar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+const menus = ['File', 'Analysis', 'View', 'Settings', 'Help'];
+
+interface MenuBarProps {
+  onOpenFileUpload: () => void;
+}
+
+export function MenuBar({ onOpenFileUpload }: MenuBarProps) {
+
+  return (
+    <div className="flex items-center h-7 px-2 border-b border-border bg-background">
+        <button
+          onClick={onOpenFileUpload}
+          className="px-3 h-full text-xs text-foreground hover:bg-card rounded-sm transition-colors"
+        >
+          File
+        </button>
+      <Link href="/profile">
+          <button
+            onClick={onOpenFileUpload}
+            className="px-3 h-full text-xs text-foreground hover:bg-card rounded-sm transition-colors"
+          >
+            Account
+          </button>
+      </Link>
+
+    </div>
+  );
+}
