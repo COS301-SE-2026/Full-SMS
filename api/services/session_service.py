9f201cf251e9915b665488573cca5669b3d9eec1
NomziPhosa <157846833+NomziPhosa@users.noreply.github.com>
2026-07-05 03:35:30 +0200

    feat: add the save_session function in session_service.py

--- api/services/session_service.py
+++ api/services/session_service.py
@@ -15,4 +15,13 @@ if not all([SUPABASE_URL, SUPABASE_SERVICE_KEY]):
 #Initialize Supabase Client
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
 
+#saving a session
+def save_session(user_id:str,session:SessionCreate)->dict:
+    new_row = {'user_id':user_id,
+                'name':session_name,
+                'dataset_ref':session.dataset_ref,
+                'parameters':session.parameters,
+                'results':session.results}
+    supabase.table('sessions_table').insert(new_row)
+    return new_row
 
