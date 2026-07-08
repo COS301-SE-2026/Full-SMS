6b3a33259f451f26fd1581469d9249444e34c2b6
NomziPhosa <157846833+NomziPhosa@users.noreply.github.com>
2026-07-08 17:44:08 +0200

    feat: add the handle_get_session_by_id function session_controller.py

--- api/controllers/session_controller.py
+++ api/controllers/session_controller.py
@@ -19,5 +19,16 @@ def handle_get_sessions(user_id:str)->list:
         return result
     except Exception as e:
         raise HTTPException(status_code=500,detail=str(e))
-
+   
+#implementing the handle_get_session_by_id() function
+def handle_get_session_by_id(session_id:str,user_id:str)->dict:
+    try:
+        result = get_session_by_id(session_id,user_id)
+        if not result:
+            raise HTTPException(status_code=404,detail="Session not found")
+        return result
+    except HTTPException:
+        raise
+    except Exception as e:
+        raise HTTPException(status_code=500,detail=str(e))
 
