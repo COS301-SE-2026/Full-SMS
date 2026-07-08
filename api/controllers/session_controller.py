dee596d3eab8ec83eaae244aa3b395bc3a610964
NomziPhosa <157846833+NomziPhosa@users.noreply.github.com>
2026-07-08 17:41:56 +0200

    feat: add the handle_save_session() function in session_controller.py

--- api/controllers/session_controller.py
+++ api/controllers/session_controller.py
@@ -2,3 +2,14 @@
 from fastapi import HTTPException
 from models.session import SessionCreate
 from services.session_service import save_session, get_sessions, get_session_by_id
+
+
+#implementing the handle_save_session() function
+def handle_save_session(user_id:str,session:SessionCreate)->dict:
+    try:
+        result = save_session(user_id,session)
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500,detail=str(e))
+
+
