7270f2b44d444ab87551b35b4037c9282f5275d9
makopimbalithobejane@gmail.com <makopimbalithobejane@gmail.com>
2026-07-10 00:23:01 +0200

    defined the endpoint function

--- api/routes/export_routes.py
+++ api/routes/export_routes.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Response, File
 from models.export_request import ExportRequest
-#add import after controller from controllers.export_controller import handle_export
+from controllers.export_controller import handle_export
 
 router = APIRouter(prefix= "/export", tags=["export"])
 
+@router.post("/")
+async def export_data(request: ExportRequest):
+    return await handle_export(request)
