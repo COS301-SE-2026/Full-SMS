2d9b21c74d55cd5b42800879d32c925bbc22e390
makopimbalithobejane@gmail.com <makopimbalithobejane@gmail.com>
2026-07-11 22:25:27 +0200

    add user_id param to handle_export

--- api/controllers/export_controller.py
+++ api/controllers/export_controller.py
@@ -4,7 +4,7 @@ from fastapi import HTTPException, BackgroundTasks
 from fastapi.responses import FileResponse
 from api.models.export_request import ExportRequest
 
-async def handle_export(request: ExportRequest, background_tasks: BackgroundTasks):
+async def handle_export(request: ExportRequest, background_tasks: BackgroundTasks, user_id: str):
     if not any([request.export_intensity, request.export_levels, request.export_groups]):
         raise HTTPException(status_code = 400, detail= "No export category selected.")
     
