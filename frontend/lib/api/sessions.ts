2f04f1e7febebfb6cb5c1e2f3e87255a89ec717b
NomziPhosa <157846833+NomziPhosa@users.noreply.github.com>
2026-07-14 12:31:38 +0200

    feat: add getSessions function in sessions.ts

--- frontend/lib/api/sessions.ts
+++ frontend/lib/api/sessions.ts
@@ -3,9 +3,19 @@ import axiosInstance from "./axiosInstance";
 //post for saving a session
 export const saveSession = async(userId: string, sessionData: object) =>{
     try{
-        const response = await axiosInstance.post('/api/py/sessions',sessionData, {params: {user_id: userId}})
+        const response = await axiosInstance.post(`/api/py/sessions`,sessionData, {params: {user_id: userId}})
         return response.data
     }catch (error){
+        throw error
+    }
+}
+
+//get sessions
+export const getSessions = async(userId: string) =>{
+    try{
+        const response = await axiosInstance.get(`/api/py/sessions`, {params: {user_id: userId}})
+        return response.data
+    }catch(error){
         throw error
     }
 }
