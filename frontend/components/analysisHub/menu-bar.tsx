9368d4674aef3ef712a06be75d32124ed9740e57
NomziPhosa <157846833+NomziPhosa@users.noreply.github.com>
2026-07-16 15:33:16 +0200

    feat: add recentsessions button in menu-bar.tsx

--- frontend/components/analysisHub/menu-bar.tsx
+++ frontend/components/analysisHub/menu-bar.tsx
@@ -2,6 +2,9 @@ import Link from "next/link";
 import { SaveSessionModal } from "./save-session-modal";
 import { useState } from "react";
 import { saveSession } from "@/lib/api/sessions";
+import { supabase } from "@/lib/supabase/supabaseConfig";
+import { RecentSessionsModal } from "./recent-sessions";
+
 interface MenuBarProps {
   onOpenFileUpload: () => void;
 }
@@ -9,9 +12,12 @@ interface MenuBarProps {
 export function MenuBar({ onOpenFileUpload }: MenuBarProps) {
   const [saveModalOpen, setSaveModalOpen] = useState(false)
   const callSave = async (name: string) => {
-    await saveSession('placeholder-user', {name: name, dataset_ref: '', parameters: {}, results: {}})
+    const {data: {user}} = await supabase.auth.getUser()
+    await saveSession(user?.id || 'anonymous', {name: name, dataset_ref: '', parameters: {}, results: {}})
     setSaveModalOpen(false)
   }
+  const [recentSessionsModalOpen, setRecentSessionsModalOpen] = useState(false)
+
   return (
     <>
     <div className="flex items-center h-7 px-2 border-b border-border bg-background">
@@ -29,6 +35,14 @@ export function MenuBar({ onOpenFileUpload }: MenuBarProps) {
         >
           Save
         </button>
+
+        <button
+          onClick={() => setRecentSessionsModalOpen(true)}
+          className="px-3 h-full text-xs text-foreground hover:bg-card rounded-sm transition-colors"
+        >
+          Sessions
+        </button>
+
       <Link href="/profile">
           <button
             onClick={onOpenFileUpload}
@@ -44,6 +58,11 @@ export function MenuBar({ onOpenFileUpload }: MenuBarProps) {
         controlClose={() => setSaveModalOpen(false)}
         controlSave={callSave}
       />
+
+      <RecentSessionsModal
+        open={recentSessionsModalOpen}
+        onClose={() => setRecentSessionsModalOpen(false)}
+        />
     </>
   );
 }
