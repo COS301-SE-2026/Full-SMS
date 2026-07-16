05f87e217d1a9f70f04060a7c88451066d60ff0f
NomziPhosa <157846833+NomziPhosa@users.noreply.github.com>
2026-07-16 15:32:39 +0200

    feat: add recentsessionsmodal function

--- frontend/components/analysisHub/recent-sessions.tsx
+++ frontend/components/analysisHub/recent-sessions.tsx
@@ -9,3 +9,37 @@ interface RecentSessionsProps{
     open: boolean
     onClose: () => void
 }
+
+export function RecentSessionsModal({open, onClose}:RecentSessionsProps){
+    const[sessions, setSessions] = useState([])
+    const[isFetching, setIsFetching] = useState(false)
+    
+    useEffect(() => {
+       const processSessions = async () => {
+        setIsFetching(true)
+        const user = await supabase.auth.getUser()
+        const userId = user.data.user?.id
+        if (!userId) return
+        const data = await getSessions(userId)
+        setSessions(data)
+        setIsFetching(false)
+       }
+       if(open) processSessions()
+    }, [open]);
+    return(
+        <Modal open={open} onClose={onClose}>
+            <div>
+                <h3>Recent Sessions</h3>
+                {isFetching && <p>Sessions Loading...</p>}
+                {sessions.map((session) => (
+                    <div key={session.id}>
+                        <p>{session.created_at}</p>
+                        <p>{session.user_id}</p>
+                        <p>{session.name}</p>
+                        <p>{session.dataset_ref}</p>
+                    </div>
+                ))}
+            </div>
+        </Modal>
+    );
+}
