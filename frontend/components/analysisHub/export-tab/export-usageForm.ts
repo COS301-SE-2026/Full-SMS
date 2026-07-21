1feecd316d11b9bfacbd3e74a63a410b9cfad71e
makopimbalithobejane@gmail.com <makopimbalithobejane@gmail.com>
2026-07-22 00:23:14 +0200

    added export options for data

--- frontend/components/analysisHub/export-tab/export-usageForm.ts
+++ frontend/components/analysisHub/export-tab/export-usageForm.ts
@@ -1,6 +1,7 @@
 "use client"
 import { useState } from "react";
 import { useHdf5Data } from "@/contexts/hdf5Context/Hdf5DataContext";
+import { ExportFormat, PLotFormat } from "./File_formats";
 
 export function useExportform() {
     const {
@@ -12,9 +13,21 @@ export function useExportform() {
         clearSelectedMeasurements,
     } = useHdf5Data();
 
+    const [exportIntensity, setExportIntensity] = useState(true);
+    const [exportLevels, setLevels] = useState(true);
+    const [exportGroups, setGroups] = useState(true);
+
+
     return {
         selectedMeasurements,
         selectAllmeasurements,
         clearSelectedMeasurements,
+        exportIntensity,
+        setExportIntensity,
+        exportLevels,
+        setLevels,
+        exportGroups,
+        setGroups,
     };
+
 }
