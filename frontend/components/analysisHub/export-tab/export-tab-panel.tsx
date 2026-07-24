ee9a38678e15423e5071c183fb774773f3084932
makopimbalithobejane@gmail.com <makopimbalithobejane@gmail.com>
2026-07-24 19:18:57 +0200

    added plot export card and checkbox options

--- frontend/components/analysisHub/export-tab/export-tab-panel.tsx
+++ frontend/components/analysisHub/export-tab/export-tab-panel.tsx
@@ -36,6 +36,22 @@ export default function ExportPanel() {
                         <Checkbox label= "Groups (clusters)" checked={form.exportGroups} onCheckedChange={form.setGroups} />
                         </CardContent>
                     </Card>
+
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Plot Export</CardTitle>
+                        </CardHeader>
+                        <CardContent className="flex flex-col gap-3">
+                            <Checkbox label= "Intensity plot" checked={form.plotIntensity} onCheckedChange={form.setPlotIntensity} />
+                            <div className="ml-6 flex flex-col gap-2">
+                                <Checkbox label= "Include Levels" checked={form.plotIncludeLevels} onCheckedChange={form.setPlotIncludeLevels} disabled={!form.plotIntensity}/>
+                                <Checkbox label= "Include Groups" checked={form.plotIncludeGroups} onCheckedChange={form.setPlotIncludeGroups} disabled={!form.plotIntensity}/>
+                            </div>
+
+                            <Checkbox label= "BIC Plot" checked={form.BICPlot} onCheckedChange={form.setBICPlot} />
+
+                        </CardContent>
+                    </Card>  
                 </div>
             </main>
         </div>
