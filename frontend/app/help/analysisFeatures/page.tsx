22f1166ae9a77619afa8b9be822696d545fb91c2
NomziPhosa <157846833+NomziPhosa@users.noreply.github.com>
2026-07-30 00:05:48 +0200

    feat: add analysis feature for the help menu

---
+++ frontend/app/help/analysisFeatures/page.tsx
@@ -0,0 +1,139 @@
+"use client"
+
+import { Card, CardHeader, CardTitle, CardDescription, CardContent} from "@/components/ui/Card"
+import { MenuBar } from "@/components/analysisHub/menu-bar"
+import { useState } from "react";
+import { ChevronDown, CircleCheckBig } from "lucide-react";
+import { Button } from "@/components/ui";
+
+export default function FAQPage(){
+    const [openQuestion, setOpenQuestion] = useState<number | null>(0)
+    return(
+        <main>
+            <MenuBar  onOpenFileUpload={()=>{}}/> 
+                <div className="flex flex-col items-center justify-center text-center py-24">
+                    <span className="text-sm uppercase tracking-wide block text-primary/70">
+                        FULL SMS Guide
+                    </span>
+                    <h1 className="text-primary text-4xl md:text-6xl font-bold">How can we help you?</h1>
+                    <div className="flex flex-row items-center justify-center text-center gap-1">
+                        <p className="text-foreground/60">Learn the basics of FULLSMS, saving a session, exporting files, adding your plugin algorithms and more.</p>
+                    </div>
+                </div>
+
+            <section className="px-4 mx-auto max-w-3xl py-2">
+                <div className="space-y-4">
+                    <Card>
+                    <CardHeader className="flex flex-row items-center justify-between bg-primary/10">
+                        <div>
+                            <CardTitle >1. What Change Point Analysis does</CardTitle>
+                        </div>
+                        <Button variant="secondary" size="sm"
+                            onClick={() => {
+                                if (openQuestion === 0) {setOpenQuestion(null)}
+                                else {setOpenQuestion(0)}
+                            }}
+                            className={`transition-transform duration-300 ${openQuestion === 0 ? "rotate-180" : ""}`}>
+                            <ChevronDown />
+                        </Button>
+                    </CardHeader>
+
+                    {openQuestion === 0 && (
+                        <CardContent>
+                            <ul className="space-y-2 list-none pl-0">
+                                <li className="flex items-start gap-2">
+                                    <CircleCheckBig className="w-4 h-4 text-primary mt-0.5 shrink-0"/>
+                                    Finds the exact moments when the behavior, pattern or average of a data set shifts significantly</li>
+                                <li className="flex items-start gap-2">
+                                    <CircleCheckBig className="w-4 h-4 text-primary mt-0.5 shrink-0"/>Divides a long data stream into distinct segments</li>
+                            </ul>
+                        </CardContent>
+                    )}
+                </Card>
+
+                <Card>
+                    <CardHeader className="flex flex-row items-center justify-between bg-primary/10">
+                        <div>
+                            <CardTitle>2. What Clustering/Grouping does</CardTitle>
+                        </div>
+                        <Button variant="secondary" size="sm"
+                            onClick={() => {
+                                if (openQuestion === 1) {setOpenQuestion(null)}
+                                else {setOpenQuestion(1)}
+                            }}
+                            className={`transition-transform duration-300 ${openQuestion === 1 ? "rotate-180" : ""}`}>
+                            <ChevronDown />
+                        </Button>
+                    </CardHeader>
+
+                    {openQuestion === 1 && (
+                        <CardContent>
+                            <ul className="space-y-2 list-none pl-0">
+                                <li className="flex items-start gap-2">
+                                    <CircleCheckBig className="w-4 h-4 text-primary mt-0.5 shrink-0"/>Groups similar data points based on shared qualities</li>
+                                <li className="flex items-start gap-2">
+                                    <CircleCheckBig className="w-4 h-4 text-primary mt-0.5 shrink-0"/>Groups intensity levels in this context</li>
+                            </ul>
+                        </CardContent>
+                    )}
+                </Card>
+
+                <Card>
+                    <CardHeader className="flex flex-row items-center justify-between bg-primary/10">
+                        <div>
+                            <CardTitle>3. What Raster Analysis does</CardTitle>
+                        </div>
+                        <Button variant="secondary" size="sm"
+                            onClick={() => {
+                                if (openQuestion === 2) {setOpenQuestion(null)}
+                                else {setOpenQuestion(2)}
+                            }}
+                            className={`transition-transform duration-300 ${openQuestion === 2 ? "rotate-180" : ""}`}>
+                            <ChevronDown />
+                        </Button>
+                    </CardHeader>
+
+                    {openQuestion === 2 && (
+                        <CardContent>
+                            <ul className="space-y-2 list-none pl-0">
+                                <li className="flex items-start gap-2">
+                                    <CircleCheckBig className="w-4 h-4 text-primary mt-0.5 shrink-0"/>Processes grid-based pixel data in geographic information systems (GIS) to evaluate continuous phenomena, perform cell-by-cell math, and model environmental processes.</li>
+                                <li className="flex items-start gap-2">
+                                    <CircleCheckBig className="w-4 h-4 text-primary mt-0.5 shrink-0"/> It examines values assigned to grid cells to study terrain, track environmental changes, and interpret satellite imagery</li>
+                            </ul>
+                        </CardContent>
+                    )}
+                </Card>
+
+                <Card>
+                    <CardHeader className="flex flex-row items-center justify-between bg-primary/10">
+                        <div>
+                            <CardTitle>4. What Spectra does</CardTitle>
+                        </div>
+                        <Button variant="secondary" size="sm"
+                            onClick={() => {
+                                if (openQuestion === 3) {setOpenQuestion(null)}
+                                else {setOpenQuestion(3)}
+                            }}
+                            className={`transition-transform duration-300 ${openQuestion === 3 ? "rotate-180" : ""}`}>
+                            <ChevronDown />
+                        </Button>
+                    </CardHeader>
+
+                    {openQuestion === 3 && (
+                        <CardContent>
+                            <ul className="space-y-2 list-none pl-0">
+                                <li className="flex items-start gap-2">
+                                    <CircleCheckBig className="w-4 h-4 text-primary mt-0.5 shrink-0"/>Spectral analysis or spectrum analysis is analysis in terms of a spectrum of frequencies or related quantities such as energies, eigenvalues, etc.</li>
+                                <li className="flex items-start gap-2">
+                                    <CircleCheckBig className="w-4 h-4 text-primary mt-0.5 shrink-0"/>Spectroscopy in chemistry and physics, a method of analyzing the properties of matter from their electromagnetic interactions</li>
+                            </ul>
+                        </CardContent>
+                    )}
+                </Card>
+                </div>
+                
+            </section>
+        </main>
+    )
+}
