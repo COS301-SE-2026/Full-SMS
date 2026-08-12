49ba8210c6c1e9910fff91df56dc4cd375ea7cd0
carlos-juma <u22602047@tuks.co.za>
2026-08-12 15:51:49 +0200

    fix: removed useState inside useEffect

--- frontend/components/ui/ThemeToggle.tsx
+++ frontend/components/ui/ThemeToggle.tsx
@@ -1,38 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import { Button } from './Button';
-import { Sun } from 'lucide-react';
+'use client'
+import React, { useEffect, useState } from "react";
+import { Button } from "./Button";
+import { Sun } from "lucide-react";
 
 interface ThemeProps {
-  type?: "button" | "icon" 
+  type?: "button" | "icon";
 }
-export default function ThemeToggle({type}:ThemeProps) {
-    const [lightMode, setLightMode] = useState(false);
-
-    useEffect(()=>{
-        const currentTheme = localStorage.getItem('theme');
-        if(currentTheme === 'light-mode'){
-            document.documentElement.classList.add('light')
-            setLightMode(true);
-        }
-    },[])
-
-    const toggleTheme = () => {
-    if (lightMode) {
-      document.documentElement.classList.remove('light');
-      localStorage.setItem('theme', 'dark');
-      setLightMode(false);
-    } else {
-      document.documentElement.classList.add('light');
-      localStorage.setItem('theme', 'light');
-      setLightMode(true);
+export default function ThemeToggle({ type }: ThemeProps) {
+  const [isLight, setIsLight] = useState(() => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("theme") === "light"
     }
-  };
-  return (
-    (type==="button" ? <Button onClick={toggleTheme} variant='ghost' className="px-3 h-full text-xs text-foreground hover:bg-card rounded-sm transition-colors" type='button'>
-     Theme
-    </Button> : <Button onClick={toggleTheme} variant='ghost' className="p-2" type='button' leftIcon={<Sun/>}>
-      
-    </Button>)
-  )
-  
+    return false
+  })
+
+  useEffect(() => {
+    if (isLight) {
+      document.documentElement.classList.add("light")
+    } 
+    else {
+      document.documentElement.classList.remove("light")
+    }
+  }, [isLight])
+
+  const toggleTheme = () => {
+    setIsLight((prev) => {
+      const nextTheme = !prev
+      localStorage.setItem("theme", nextTheme ? "light" : "dark")
+      return nextTheme
+    })
+  }
+  return type === "button" ? (
+    <Button
+      onClick={toggleTheme}
+      variant="ghost"
+      className="px-3 h-full text-xs text-foreground hover:bg-card rounded-sm transition-colors"
+      type="button"
+    >
+      Theme
+    </Button>
+  ) : (
+    <Button
+      onClick={toggleTheme}
+      variant="ghost"
+      className="p-2"
+      type="button"
+      leftIcon={<Sun />}
+    ></Button>
+  );
 }
