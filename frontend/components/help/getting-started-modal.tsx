f159d642b3ee63ec015483a7a09d18d372d53fee
NomziPhosa <157846833+NomziPhosa@users.noreply.github.com>
2026-08-27 13:16:09 +0200

    feat: add a return function in the gettingStarted modal file

--- frontend/components/help/getting-started-modal.tsx
+++ frontend/components/help/getting-started-modal.tsx
@@ -23,3 +23,28 @@ export function GettingStartedModal({open, onClose}: GettingStartedProps){
             points: ["Click the Workspaces option in the side menu","Select the file you uploaded","You will be led to the Analysis Hub page","Select an analysis algorithm or any from your plugins","Provide necessary parameter values","Select your preferred type of measurements"]
         }
     ]
+    const max = steps.length - 1
+    return(
+        <Modal open={open} onClose={onClose}>
+
+            <h3>{steps[currentStep].title}</h3>
+            <ul>
+                {steps[currentStep].points.map((point, index) => (
+                    <li key={index}>{point}</li>
+                ))}
+            </ul>
+            <span>Step {currentStep+1} of {steps.length}</span>
+            <div className="flex gap-2 justify-end">
+                <Button
+                onClick={() => setCurrentStep(currentStep > 0 ? currentStep - 1 : currentStep)}>Back</Button>
+                <Button
+                onClick={() => {if(currentStep === max){onClose();}
+                else{
+                setCurrentStep(currentStep+1)}}}
+                >{currentStep === max ? "Finish": "Next"}</Button>
+            </div>
+        </Modal>
+        
+    );
+    
+}
