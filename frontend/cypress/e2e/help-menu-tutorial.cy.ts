8dafe947c8ba6f892d921c0f5e556aaa2157cfaa
NomziPhosa <157846833+NomziPhosa@users.noreply.github.com>
2026-09-01 16:39:02 +0200

    feat: test if step 1 has the righ title

--- frontend/cypress/e2e/help-menu-tutorial.cy.ts
+++ frontend/cypress/e2e/help-menu-tutorial.cy.ts
@@ -4,3 +4,7 @@ describe('Help Menu Tutorial', () => {
         cy.visit('/help')
         cy.contains('Getting Started').click({force: true})
     })
+
+    it('step 1 contains How to create a workspace', () => {
+        cy.get('[data-cy="step-tutorial-title"]').should('contain', 'How to create a workspace')
+    })
