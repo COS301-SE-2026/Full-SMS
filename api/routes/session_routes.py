cf4c49749a0cd8aa0dd70f3bcbd1020e1e1cb434
NomziPhosa <157846833+NomziPhosa@users.noreply.github.com>
2026-09-09 15:27:02 +0200

    fix: add changes in session routes and sessions services

--- api/routes/session_routes.py
+++ api/routes/session_routes.py
@@ -1,20 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from typing import Annotated
+from api.routes.profile_routes import get_current_user
 from api.controllers.session_controller import handle_save_session, handle_get_sessions, handle_get_session_by_id
 from api.models.session import SessionCreate
 
 session_router = APIRouter(prefix="/sessions",tags=["sessions"])
 
-
 @session_router.post("/")
-def save_session(session:SessionCreate,user_id:str):
+def save_session(session:SessionCreate, current_user: Annotated[dict, Depends(get_current_user)]):
+    user_id = current_user["user"]["id"]
     return handle_save_session(user_id,session)
 
 @session_router.get("/")
-def get_sessions(user_id:str):
+def get_sessions(current_user: Annotated[dict, Depends(get_current_user)]):
+    user_id = current_user["user"]["id"]
     return handle_get_sessions(user_id)
 
 @session_router.get("/{session_id}")
-def get_session_by_id(session_id:str,user_id:str):
+def get_session_by_id(session_id:str, current_user: Annotated[dict, Depends(get_current_user)]):
+    user_id = current_user["user"]["id"]
     return handle_get_session_by_id(session_id,user_id)
 
 
--- frontend/services/sessionsServices.ts
+++ frontend/services/sessionsServices.ts
@@ -3,7 +3,7 @@ import axiosInstance from "@/lib/api/axiosInstance"
 export const sessionsService = {
     saveSession: async(userId: string, sessionData: object) =>{
         try{
-            const response = await axiosInstance.post(`/api/py/sessions/`,sessionData, {params: {user_id: userId}})
+            const response = await axiosInstance.post(`/api/py/sessions/`,sessionData)
             return response.data
         }catch(error: any){
             throw new Error(
@@ -14,7 +14,7 @@ export const sessionsService = {
 
     getSessions: async(userId: string) =>{
         try{
-            const response = await axiosInstance.get(`/api/py/sessions`, {params: {user_id: userId}})
+            const response = await axiosInstance.get(`/api/py/sessions`)
             return response.data
         }catch(error: any){
             throw new Error(
@@ -23,9 +23,9 @@ export const sessionsService = {
         }
     },
 
-    getSessionsById: async(userId: string, sessionId: string) =>{
+    getSessionsById: async(sessionId: string) =>{
         try{
-            const response = await axiosInstance.get(`/api/py/sessions/${sessionId}`, {params: {user_id: userId}})
+            const response = await axiosInstance.get(`/api/py/sessions/${sessionId}`)
             return response.data
         }catch(error: any){
             throw new Error(
