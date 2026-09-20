dce100869ffac1a1f0fea483321388b7d27925e4
makopimbalithobejane@gmail.com <makopimbalithobejane@gmail.com>
2026-09-20 20:57:33 +0200

    added route for listing parameter history

--- api/routes/parameter_history_routes.py
+++ api/routes/parameter_history_routes.py
@@ -2,10 +2,9 @@ from fastapi import APIRouter, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from typing import Annotated
 from api.controllers.auth_controller import verify_token_controller
-
-
-from api.models.workspace import WorkspaceCreate, WorkspaceUpdate
-from typing import Annotated
+from api.controllers.parameter_history_controller import(
+    list_history_controller,
+)
 
 router = APIRouter(prefix="/workspaces", tags=["Parameter History"])
 bearer_scheme = HTTPBearer()
@@ -16,3 +15,7 @@ def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(bearer_
     return result["user"]
 
 CurrentUser = Annotated[dict, Depends(get_current_user)] 
+
+@router.get("/{workspace_id}/history", summary="List parameter history")
+def list_history_route(workspace_id: str, upload_id: str, tab: str, current_user:CurrentUser):
+    return list_history_controller(workspace_id, current_user["id"], upload_id, tab)
