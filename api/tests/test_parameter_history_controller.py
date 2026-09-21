29dfb091f208863cc7ed3e99d06894c21d1801ca
makopimbalithobejane@gmail.com <makopimbalithobejane@gmail.com>
2026-09-21 17:46:44 +0200

    added test to verify that list history controller returns 500

--- api/tests/test_parameter_history_controller.py
+++ api/tests/test_parameter_history_controller.py
@@ -14,7 +14,13 @@ class TestListHistoryController:
 
 
     def test_workspace_notFound(self):
-            with patch(PATH, side_effect=ValueError("Workspace not found")):
-                from api.controllers.parameter_history_controller import list_history_controller
-                with pytest.raises(HTTPException) as exc: list_history_controller("ws1", "user1", "u1", "intensity")
-            assert exc.value.status_code == 404
+        with patch(PATH, side_effect=ValueError("Workspace not found")):
+            from api.controllers.parameter_history_controller import list_history_controller
+            with pytest.raises(HTTPException) as exc: list_history_controller("ws1", "user1", "u1", "intensity")
+        assert exc.value.status_code == 404
+
+    def test_sreviceFailure(self):
+        with patch(PATH, side_effect=RuntimeError("Unexpected service failure")):
+            from api.controllers.parameter_history_controller import list_history_controller
+            with pytest.raises(HTTPException) as exc: list_history_controller("ws1", "user1", "u1", "intensity")
+        assert exc.value.status_code == 500
