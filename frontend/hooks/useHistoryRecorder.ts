d81e57fef7ef266161a488098670dd71551b30a3
makopimbalithobejane@gmail.com <makopimbalithobejane@gmail.com>
2026-09-24 09:40:21 +0200

    added tracking of first parameter value

--- frontend/hooks/useHistoryRecorder.ts
+++ frontend/hooks/useHistoryRecorder.ts
@@ -1,13 +1,19 @@
 import { HistoryTab } from "@/types/parameterHistory";
-import { useCallback } from "react";
+import { useCallback,useRef } from "react";
 
 export function usehistoryRecorder(
     workspaceId: string | null,
     uploadId: string,
     tab: HistoryTab,
 ){
+    const firstValues = useRef<Record<string, number>>({});
+
     return useCallback(
-        (parameter: string, newValue: number) => {},
+        (parameter: string, newValue: number) => {
+            if(!(parameter in firstValues.current)){
+                firstValues.current[parameter] = newValue;
+            }
+        },
         [workspaceId, uploadId, tab],
     ); 
 }
