1e9aaa854fa7bfcd78113bf2ef142a9898cf4ef9
makopimbalithobejane@gmail.com <makopimbalithobejane@gmail.com>
2026-09-26 01:46:14 +0200

    connnected lifetime tab to parameter hsitory

--- frontend/components/analysisHub/lifetime-tab/lifetime-tab.tsx
+++ frontend/components/analysisHub/lifetime-tab/lifetime-tab.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import LifetimeToolbar from './lifetime-toolbar'
 import LifetimeCharts from './lifetime-charts'
+import { useHistory } from '@/hooks/useHistory'
+import { HistoryPanel } from '../history/HistoryPanel'
+import { useHdf5Data } from '@/contexts/hdf5Context/Hdf5DataContext'
 
 export default function LifetimeTab() {
+  const{ currentWorkspaceId, currentUpload, currentMeasurement} = useHdf5Data()
+  const {entries, loading, error, fetchHistory}= useHistory(currentWorkspaceId, currentUpload, "lifetime")
 
 
   return (
