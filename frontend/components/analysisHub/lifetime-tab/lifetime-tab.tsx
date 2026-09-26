cfe8b284abef3fd3372f1bfdd8368a1e71d0605f
makopimbalithobejane@gmail.com <makopimbalithobejane@gmail.com>
2026-09-26 02:27:07 +0200

    added history panel to lifetime tab' '

--- frontend/components/analysisHub/lifetime-tab/lifetime-tab.tsx
+++ frontend/components/analysisHub/lifetime-tab/lifetime-tab.tsx
@@ -4,16 +4,45 @@ import LifetimeCharts from './lifetime-charts'
 import { useHistory } from '@/hooks/useHistory'
 import { HistoryPanel } from '../history/HistoryPanel'
 import { useHdf5Data } from '@/contexts/hdf5Context/Hdf5DataContext'
+import { useAnalysisTab } from '@/contexts/analysisTabsContext/AnalysisTabsContext'
+import { getLifetimeData } from '@/services/analysisServices'
+import { upload } from '@testing-library/user-event/dist/cjs/utility/upload.js'
+import { historyService } from '@/services/historyServices'
 
 export default function LifetimeTab() {
   const{ currentWorkspaceId, currentUpload, currentMeasurement} = useHdf5Data()
   const {entries, loading, error, fetchHistory}= useHistory(currentWorkspaceId, currentUpload, "lifetime")
-
+  const { decayCounts, decayTimes, setFitResult} = useAnalysisTab()
 
   return (
-    <div className='w-full h-full'>
+    <div className='w-full h-full flex gap-3'>
+      <div className='flex flex-col flex-1'>
         <LifetimeToolbar/>
         <LifetimeCharts/>
+      </div>
+
+      <HistoryPanel
+        entries={entries}
+        loading={loading}
+        error={error}
+        onRevert={async (entry) => {
+          const response = await getLifetimeData({
+            ...entry.old_value,
+            upload_id: currentUpload,
+            measurement_id: currentMeasurement,
+            times: decayTimes,
+            counts: decayCounts,
+          })
+          setFitResult(response)
+
+          await historyService.revertEntry(
+            currentWorkspaceId!,
+            entry.id
+          )
+
+          fetchHistory()
+        }}
+      />
     </div>
   )
 }
