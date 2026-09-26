ebd3239620ed2c81f078972b96b60b6eb6f663b3
makopimbalithobejane@gmail.com <makopimbalithobejane@gmail.com>
2026-09-26 13:20:03 +0200

    connected correlation tab to history

--- frontend/components/analysisHub/correlation-tab/correlation-tab.tsx
+++ frontend/components/analysisHub/correlation-tab/correlation-tab.tsx
@@ -3,12 +3,19 @@ import CorrelationTabToolbar from "./correlation-tab-toolbar";
 import Plot from "react-plotly.js";
 import { Card } from "@/components/ui";
 import CorrelationChart from "./correlation-chart";
+import { useHdf5Data } from "@/contexts/hdf5Context/Hdf5DataContext";
+import { useHistory } from "@/hooks/useHistory";
 
 function CorrelationTab() {
+  const { currentWorkspaceId, currentUpload, currentMeasurement, setCorrelationData} = useHdf5Data();
+  const {entries, loading, error, fetchHistory} = useHistory(currentWorkspaceId, currentUpload, "correlation");
+  
   return (
-    <div >
-      <CorrelationTabToolbar />
-      <CorrelationChart/>
+    <div className="w-full h-full flex gap-3">
+      <div className="flex flex-col flex-1">
+        <CorrelationTabToolbar />
+        <CorrelationChart/>
+      </div>
     </div>
   );
 }
