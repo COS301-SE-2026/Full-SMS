8d6c4297d2ee9687865fa2b48b35ff30b8e49669
makopimbalithobejane@gmail.com <makopimbalithobejane@gmail.com>
2026-09-26 17:16:41 +0200

    connected raster tab to history

--- frontend/components/analysisHub/raster-tab/raster-tab.tsx
+++ frontend/components/analysisHub/raster-tab/raster-tab.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import RasterToolbar from './raster-toolbar'
 import { RasterHeatmap } from './rater-heatmap'
+import { useHdf5Data } from '@/contexts/hdf5Context/Hdf5DataContext'
+import { useHistory } from '@/hooks/useHistory'
 
 export default function RasterTab() {
+  const {currentWorkspaceId, currentUpload, setHeatMapColor} = useHdf5Data()
+  const {entries, loading, error, fetchHistory} = useHistory(currentWorkspaceId, currentUpload, "raster")
   return (
-    <div className='h-full w-full'>
+    <div className='h-full w-full flex gap-3'>
+      <div className='flex flex-col flex-1'>
         <RasterToolbar/>
         <RasterHeatmap/>
     </div>
+  </div>
   )
 }
